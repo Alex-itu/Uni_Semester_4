743dc0f31b2125fa360f55d65e2eedaadb3e8747
Alexander Lolk <Lolk@itu.dk>
2024-02-08 14:14:59 +0100

    hej

--- Algo round 2/Lecture 2/Geppetto.py
+++ Algo round 2/Lecture 2/Geppetto.py
@@ -3,13 +3,16 @@ n, m = map(int, input().split())
 forbidden_pairs = set()
 for _ in range(m):
     a,b = map(int, input().split())
+    print("A: " + str(a) + "  B:  " + str(b))
     forbidden_pairs.add(tuple(sorted([a,b])))
+    print(forbidden_pairs)
 
 count = 0
 for i in range(2**n):
         #subset = f"{i:0{n}b}"
         forbidden = False
         for a,b in forbidden_pairs:
+            print("A: " + str(a) + "  B:  " + str(b))
             #if subset[a-1]=='1' and subset[b-1] == '1':
             if (i & (1 << (a-1))) and (i & (1 << (b-1))):
                 forbidden = True
